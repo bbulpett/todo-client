b3ce4d1c5659629f3ad59b67dce1837299c514e3
Barnabas Bulpett <barnabasbulpett@gmail.com>
2016-04-12 22:20:57 -0400

    Added route and basic index template for todo-widget

--- app/routes/todos.js
+++ app/routes/todos.js
@@ -1,4 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+
+	model(){
+		return [
+			{ text: "Mow the grass" },
+			{ text: "Rake the database" },
+			{ text: "Fix the television" }
+		]
+	}
 });
